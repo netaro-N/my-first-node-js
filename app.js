e4790fe5e48a581f9c7fb90790120b66f18f7f7f
netaro-N <takebbb1+github@gmail.com>
2018-10-23 02:05:53 +0000

    コマンドライン引数をコンソールに表示

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
+console.log('process.argv[0] = ' + process.argv[0]);
+console.log('process.argv[1] = ' + process.argv[1]);
+console.log('process.argv[2] = ' + process.argv[2]);
 const number = process.argv[2] || 0 ;
 let sum = 0 ;
 for (let i = 0 ; i <= number ; i++) {
